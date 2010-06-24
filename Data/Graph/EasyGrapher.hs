3a706ffbccc74cf660889400d836bd1cade8ed46
Hiromi Ishii <mr_konn@jcom.home.ne.jp>
2010-06-23 19:31:28 -0700

    add support for unconnected vertex in graph.

--- Data/Graph/EasyGrapher.hs
+++ Data/Graph/EasyGrapher.hs
@@ -11,9 +11,9 @@ import Prelude hiding (lookup)
 import Data.Generics
 
 
-data (Eq a, Ord a) => EGEdge a = a :=> a deriving (Show, Eq, Typeable)
-deriving instance (Data a, Ord a)=>Data (EGEdge a)
-type EGGraph a = [EGEdge a]
+data (Eq a, Ord a) => EGTerm a = a :=> a | EGVertex a deriving (Show, Eq, Typeable)
+deriving instance (Data a, Ord a)=>Data (EGTerm a)
+type EGGraph a = [EGTerm a]
 
 data Env gr a = Env{graph :: gr a (), dic :: Map a Node}
 empty :: (Eq a, DynGraph gr) => Env gr a
@@ -24,21 +24,23 @@ type GrMachine gr lab a = State (Env gr lab) a
 buildGraph :: (DynGraph gr, Ord a) => EGGraph a -> gr a ()
 buildGraph xs = evalState (build xs) empty
 
-build :: (Ord lab, DynGraph gr) => [EGEdge lab] -> GrMachine gr lab (gr lab ())
+build :: (Ord lab, DynGraph gr) => EGGraph lab -> GrMachine gr lab (gr lab ())
 build [] = gets graph 
 build ((lab1 :=> lab2):xs) = do
-    [n1, n2] <- mapM toNode [lab1, lab2]
-    env@Env{graph} <- get
-    put $ env{graph=insEdge (n1, n2, ()) graph}
-    build xs
-  where
-    toNode :: (Ord lab, DynGraph gr) => lab -> GrMachine gr lab Node
-    toNode lab = do
-      cond <- gets $ notMember lab . dic
-      when cond $ mkNode lab
-      gets $ fromJust . lookup lab . dic
-    mkNode :: (Ord lab, DynGraph gr) => lab -> GrMachine gr lab ()
-    mkNode lab = do
-      (nd:_) <- gets (newNodes 1 . graph)
-      env@Env{graph, dic} <- get
-      put $ env{graph=insNode (nd, lab) graph, dic=insert lab nd dic}
+  [n1, n2] <- mapM toNode [lab1, lab2]
+  env@Env{graph} <- get
+  put $ env{graph=insEdge (n1, n2, ()) graph}
+  build xs
+build ((EGVertex lab):xs) = toNode lab >> return ()
+
+toNode :: (Ord lab, DynGraph gr) => lab -> GrMachine gr lab Node
+toNode lab = do
+  cond <- gets $ notMember lab . dic
+  when cond $ mkNode lab
+  gets $ fromJust . lookup lab . dic
+
+mkNode :: (Ord lab, DynGraph gr) => lab -> GrMachine gr lab ()
+mkNode lab = do
+  (nd:_) <- gets (newNodes 1 . graph)
+  env@Env{graph, dic} <- get
+  put $ env{graph=insNode (nd, lab) graph, dic=insert lab nd dic}
