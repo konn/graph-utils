ccc3ed2d105b773eff2b4a96865a99f3058524f7
Hiromi Ishii <mr_konn@jcom.home.ne.jp>
2010-06-24 21:58:24 +0900

    Change Library Hierarchy

---
+++ Data/Graph/EasyGrapher/EasyGrapher.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NamedFieldPuns, StandaloneDeriving, DeriveDataTypeable, TypeSynonymInstances #-}
+module Data.Graph.EasyGrapher.EasyGrapher (EGGraph(..), EGTerm(..), buildGraph) where
+import Data.Graph.Inductive hiding(empty)
+import qualified Data.Graph.Inductive as G
+import Control.Monad
+import Data.Map hiding (map, empty)
+import qualified Data.Map as M
+import Control.Monad.State 
+import Data.Maybe
+import Prelude hiding (lookup)
+import Data.Generics
+import qualified Data.Graph as DG
+
+data (Eq a, Ord a) => EGTerm a = a :=> a | EGVertex a deriving (Show, Eq, Typeable)
+deriving instance (Data a, Ord a)=>Data (EGTerm a)
+type EGGraph a = [EGTerm a]
+
+data Env gr a = Env{graph :: gr a (), dic :: Map a Node}
+empty :: (Eq a, DynGraph gr) => Env gr a
+empty = Env{graph = G.empty, dic = M.empty}
+
+type GrMachine gr lab a = State (Env gr lab) a
+
+buildGraph :: (DynGraph gr, Ord a) => EGGraph a -> gr a ()
+buildGraph xs = evalState (build xs) empty
+
+build :: (Ord lab, DynGraph gr) => EGGraph lab -> GrMachine gr lab (gr lab ())
+build [] = gets graph 
+build ((lab1 :=> lab2):xs) = do
+  [n1, n2] <- mapM toNode [lab1, lab2]
+  env@Env{graph} <- get
+  put $ env{graph=insEdge (n1, n2, ()) graph}
+  build xs
+build ((EGVertex lab):xs) = toNode lab >> build xs
+
+toNode :: (Ord lab, DynGraph gr) => lab -> GrMachine gr lab Node
+toNode lab = do
+    cond <- gets $ notMember lab . dic
+    when cond $ mkNode lab
+    gets $ fromJust . lookup lab . dic
+  where
+    mkNode :: (Ord lab, DynGraph gr) => lab -> GrMachine gr lab ()
+    mkNode lab = do
+      (nd:_) <- gets (newNodes 1 . graph)
+      env@Env{graph, dic} <- get
+      put $ env{graph=insNode (nd, lab) graph, dic=insert lab nd dic}
---
+++ Data/Graph/EasyGrapher/Quote.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE TemplateHaskell, NamedFieldPuns, TupleSections  #-}
+module Data.Graph.EasyGrapher.Quote (gr) where
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+import Text.Parsec hiding ((<|>), many, State)
+import Control.Applicative
+import Data.Graph.EasyGrapher.EasyGrapher
+
+parseGraph :: (Monad m) => (String, Int, Int) -> String -> m (EGGraph String)
+parseGraph (file, line, col) src = 
+  case (parse p "" src) of
+    Left err -> fail $ show err
+    Right gr -> return gr
+  where
+    p = do
+      pos <- getPosition
+      setPosition $
+        (flip setSourceName) file $
+        (flip setSourceLine) line $
+        (flip setSourceColumn) col $
+        pos
+      spaces *> lexeme(graphs)
+
+symbol s = lexeme $ string s
+lexeme :: Parsec String u a -> Parsec String u a
+lexeme p = p <* spaces
+graphs :: Parsec String () (EGGraph String)
+graphs = sepEndBy1 term (symbol ",")
+term = try edge <|> EGVertex <$> ident
+edge = (:=>) <$> (ident<* symbol "->") <*> ident
+ident = lexeme $ many1 alphaNum
+
+gr :: QuasiQuoter
+gr = QuasiQuoter quoteGraphExp quoteGraphPat
+
+quoteGraphExp :: String -> ExpQ
+quoteGraphExp src = do
+  loc <- location
+  let pos=(loc_filename loc, fst $ loc_start loc, snd $ loc_start loc)
+  gr <- parseGraph pos src
+  appE (varE 'buildGraph) $ dataToExpQ (const Nothing) gr
+
+quoteGraphPat :: String -> PatQ
+quoteGraphPat src = do
+  loc <- location
+  let pos=(loc_filename loc, fst $ loc_start loc, snd $ loc_start loc)
+  gr <- parseGraph pos src
+  dataToPatQ (const Nothing) gr
