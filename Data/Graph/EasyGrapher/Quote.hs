3bc11893406c4736304ebb7ba18ecc58602c7c7c
Hiromi Ishii <mr_konn@jcom.home.ne.jp>
2010-06-27 00:04:37 +0900

    Fixed module names

--- Data/Graph/EasyGrapher/Quote.hs
+++ Data/Graph/EasyGrapher/Quote.hs
@@ -5,7 +5,7 @@ import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 import Text.Parsec hiding ((<|>), many, State, label)
 import Control.Applicative
-import Data.Graph.EasyGrapher
+import Data.Graph.EasyGrapher.EasyGrapher
 import Data.Generics
 import Data.List
 
